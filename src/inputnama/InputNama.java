bca40eef06f4abd8472fa970fe285ee39ef621e5
Mohammad Basmalah <puber2350@gmail.com>
2019-10-02 15:11:15 +0700

    Input Nama

---
+++ src/inputnama/InputNama.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inputnama;
+
+/**
+ *
+ * @author USER
+ */
+public class InputNama {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Menginput Nama
+        System.out.println("Nama : Mohammad Basmalah");
+        System.out.println("Usia : 19");
+        System.out.println("Tempat Lahir : Depok");
+        System.out.println("Alamat Depok");
+        
+    }
+    
+}
